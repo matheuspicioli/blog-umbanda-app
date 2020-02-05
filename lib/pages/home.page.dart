b2865d273d780aeabf9e50c70aa1220bae857b62
Matheus Picioli <matheus.picioli@ciatecnica.com.br>
2020-02-05 17:50:20 -0300

    Removido posts chumbados e capturando através da API

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -2,36 +2,39 @@ import 'package:blog_umbanda/models/post.model.dart';
 import 'package:blog_umbanda/widgets/post-item.widget.dart';
 import 'package:blog_umbanda/widgets/post-spotlight.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:blog_umbanda/services/post.service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<PostModel> posts = [];
+  List<Widget> widgets = [];
+  bool error = false;
+
+  @override
+  void initState() {
+    PostService.fetchPosts().then((List<PostModel> postsApi) {
+      print('DEBUG: Alright with fetch posts');
+      setState(() {
+        posts = postsApi;
+        for (var post in posts) {
+          widgets.add(PostItem(post: post));
+        }
+      });
+    }).catchError((onError) {
+      print('DEBUG: Error on fetch posts');
+      setState(() {
+        error = true;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<PostModel> posts = [
-      new PostModel(
-        title: "Florestas",
-        contents: [
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-            "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-            "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        ],
-        author: "Matheus Picioli",
-        imagePost:
-            "https://image.freepik.com/fotos-gratis/indian-beautiful-forest-landscapes_1376-210.jpg",
-        visualizers: 30,
-      ),
-      // new PostItemBloc(
-      //     title: "Oxóssi",
-      //     body: "Breve descrição sobre oxóssi",
-      //     imagePost:
-      //         "https://img.elo7.com.br/product/zoom/1F29224/banner-oxossi-o-cacador-banner-oxossi.jpg"),
-    ];
-    List<Widget> widgets = [];
-
-    for (var post in posts) {
-      widgets.add(PostItem(post: post));
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Blog umbanda"),
@@ -50,7 +53,15 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.all(20),
             child: Column(
-              children: widgets,
+              children: widgets.length > 0 && !error
+                  ? widgets
+                  : <Widget>[
+                      Center(
+                        child: !error
+                            ? CircularProgressIndicator()
+                            : Text('Erro na chamada da API'),
+                      ),
+                    ],
             ),
           ),
         ],
