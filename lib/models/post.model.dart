eaee6d0f8100e5831a0cb3d628db87a2a4d5d0fa
Matheus Picioli <matheus.picioli@ciatecnica.com.br>
2020-02-05 17:48:57 -0300

    Alterado atributos para nova estrutura

--- lib/models/post.model.dart
+++ lib/models/post.model.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/foundation.dart';
 
 class PostModel {
+  int id;
   String title;
-  List<String> contents;
+  String content;
+  String contentPreview;
   String author;
-  int visualizers;
+  int views;
   String imageSpotlight;
   String imagePost;
 
   PostModel({
+    @required this.id,
     @required this.title,
-    @required this.contents,
+    @required this.content,
+    @required this.contentPreview,
     @required this.author,
-    this.visualizers,
+    this.views,
     this.imageSpotlight,
     this.imagePost,
   });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      contentPreview: json['contentPreview'],
+      author: json['author'],
+      views: json['views'],
+      imageSpotlight: json['pathToImageSpotlight'],
+      imagePost: json['pathToImagePost'],
+    );
+  }
 
   getText(string, maximumCaracter) {
     if (string.length > maximumCaracter) {
